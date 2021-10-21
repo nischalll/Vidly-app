05990162c0cfa4769319faf1d692ea0b23ab1234
nischalll <nischalnischal6@gmail.com>
2021-10-21 14:43:09 +0530

    added pagination and listgroup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Movies from "./components/movies";
+
+function App() {
+  return (
+    <main className="container my-3">
+      <Movies />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/movies.jsx
@@ -0,0 +1,118 @@
+import React from "react";
+import { getMovies } from "../services/fakeMovieService";
+import Like from "./common/like";
+import Pagination from "./common/pagination";
+import { paginate } from "../utils/paginate";
+import { getGenres } from "../services/fakeGenreService";
+import ListGroup from "./common/listGroup";
+class Movies extends React.Component {
+  state = {
+    movies: [],
+    genres: [],
+    pageSize: 4,
+    currentPage: 1,
+  };
+
+  componentDidMount() {
+    const genres = [{ name: "All Genres" }, ...getGenres()];
+
+    this.setState({ movies: getMovies(), genres });
+  }
+
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  handleLike = (movie) => {
+    const movies = [...this.state.movies];
+    let index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    // movies[index].liked === true
+    //   ? (movies[index].liked = false)
+    //   : (movies[index].liked = true);
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
+
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
+
+  handleGenreSelect = (genre) => {
+    this.setState({ selectedGenre: genre });
+  };
+
+  render() {
+    const count = this.state.movies.length;
+    const { pageSize, currentPage, movies: allMovies } = this.state;
+
+    const filtered = this.state.selectedGenre
+      ? allMovies.filter((m) => m.genre._id === this.state.selectedGenre._id)
+      : allMovies;
+    const movies = paginate(filtered, currentPage, pageSize);
+    return (
+      <div className="row">
+        <div className="col-3">
+          <ListGroup
+            items={this.state.genres}
+            selectedItem={this.state.selectedGenre}
+            onItemSelect={this.handleGenreSelect}
+          />
+        </div>
+        <div className="col">
+          {" "}
+          {count === 0 && <h1>There are no movies in database</h1>}
+          {count !== 0 && (
+            <div>
+              <h5>Showing {filtered.length} movies in the database</h5>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Title</th>
+                    <th>Genre</th>
+                    <th>Stock</th>
+                    <th>Rate</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {movies.map((movie) => (
+                    <tr key={movie._id}>
+                      <td>{movie.title}</td>
+                      <td>{movie.genre.name}</td>
+                      <td>{movie.numberInStock}</td>
+                      <td>{movie.dailyRentalRate}</td>
+                      <td>
+                        <Like
+                          liked={movie.liked}
+                          onClick={() => this.handleLike(movie)}
+                        />
+                      </td>
+                      <td>
+                        <button
+                          onClick={() => this.handleDelete(movie)}
+                          className="btn btn-danger btn-sm"
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
+
+        <Pagination
+          itemsCount={filtered.length}
+          pageSize={pageSize}
+          currentPage={currentPage}
+          onPageChange={this.handlePageChange}
+        />
+      </div>
+    );
+  }
+}
+
+export default Movies;
